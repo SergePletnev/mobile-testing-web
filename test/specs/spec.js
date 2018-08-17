4202a3224f3131007a295bcd8c2c83f5a120d8ad
Siarhei Plistniou <siarhei_pliatniou@epam.com>
2018-08-17 15:21:37 +0300

    add its

--- test/specs/spec.js
+++ test/specs/spec.js
@@ -1,13 +1,25 @@
 'use strict'
 
+const baseUri = 'https://www.onliner.by/';
+const sectionsNumber = 15;
+const sections = element.all(by.css('.project-navigation__item'));
+const searchInput = element(by.css('.fast-search__input'));
+
 describe('Simple test', () => {
     beforeAll(() => {
-        browser.get('https://www.onliner.by/');
+        browser.get(baseUri);
     });
 
-    it('should open the app', () => {
+    it('should have correct title', () => {
         expect(browser.getTitle()).toEqual('Onliner.by');
-        console.log('Test Done!!!');
+    });
+   
+    it(`should contain ${sectionsNumber} catalog sections`, () => {
+        expect(sections.count()).toEqual(sectionsNumber);
+    });
+
+    it('should contain fast search input on the page', () => {
+        expect(searchInput.isDisplayed()).toBeTruthy();
     });
 
     afterAll(() => {
